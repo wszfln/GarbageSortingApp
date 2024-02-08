ef3bb14cfe9f51e44028b781369f30177a0837a7
Yingying Lu <20095278@mail.wit.ie>
2024-02-08 08:36:18 +0000

    improve login/register page UI design and add basic foundations

--- app/src/main/java/com/example/garbagesortingapp/LoginActivity.java
+++ app/src/main/java/com/example/garbagesortingapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,11 +13,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView txvTitle1;
     private TextView txvTitle2;
+    private TextView txvTitle5;
     private EditText edtEmail;
     private EditText edtPassword;
     private Button btnLogin;
+    private TextView txvTitle6;
     private Button btnTourist;
-    private Button btnCreate;
+    private TextView txvTitle7;
+    private TextView txvTitle8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,14 @@ public class LoginActivity extends AppCompatActivity {
         //Get component
         txvTitle1 = findViewById(R.id.txvTitle1);
         txvTitle2 = findViewById(R.id.txvTitle2);
+        txvTitle5 = findViewById(R.id.txvTitle5);
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        txvTitle6 = findViewById(R.id.txvTitle6);
         btnTourist = findViewById(R.id.btnTourist);
-        btnCreate = findViewById(R.id.btnCreate);
+        txvTitle7 = findViewById(R.id.txvTitle7);
+        txvTitle8 = findViewById(R.id.txvTitle8);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,11 +50,16 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        txvTitle8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Create an Intent object and jump from LoginActivity to RegisterActivity
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                // Start the Intent using the startActivity method
+                startActivity(intent);
             }
         });
+
     }
 }
+
--- app/src/main/java/com/example/garbagesortingapp/RegisterActivity.java
+++ app/src/main/java/com/example/garbagesortingapp/RegisterActivity.java
@@ -39,3 +39,4 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 }
+
